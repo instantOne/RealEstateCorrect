39a416ee175581270daef92c777ea22c7a2cdafa
Vladimir Veselov <subolgplayer@gmail.com>
2024-05-09 12:34:09 +0300

    Created project

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# from config import SQLALCHEMY_DATABASE_URL
+from config import SQLALCHEMY_DATABASE_URL
 
 
-#
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL
-# )
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#
-# Base = declarative_base()
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
